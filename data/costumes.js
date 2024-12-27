3f2985c155432d926a25357eecc26c4d60defaf4
Flexer144 <lopatanikita61@gmail.com>
2024-12-27 12:09:35 +0300

    Закончил добавление карточек костюмов

--- data/costumes.js
+++ data/costumes.js
@@ -1,13 +1,13 @@
 export const suit = [
 {
-  id: '21289791',
+  id: '2128979153',
   image: 'imageSuit/spiderMan.png',
   name: 'Человек-паук',
   price: '1000',
   stock: 1
 },
 {
-  id: '236389791',
+  id: '2363897913',
   image: 'imageSuit/Batman.png',
   name: 'Бэтмен',
   price: '1500',
@@ -26,5 +26,61 @@ export const suit = [
   name: 'Оби Ван Кеноби',
   price: '1000',
   stock: 1
+},
+{
+  id: '3123643791',
+  image: 'imageSuit/Captain America.png',
+  name: 'Капитан Америка',
+  price: '1500',
+  stock: 2
+},
+{
+  id: '3583243091',
+  image: 'imageSuit/Darth Vader.png',
+  name: 'Дарт Вейдер',
+  price: '1500',
+  stock: 3
+},
+{
+  id: '3461244391',
+  image: 'imageSuit/Jedi.png',
+  name: 'Джедай',
+  price: '1500',
+  stock: 0
+},
+{
+  id: '3551200391',
+  image: 'imageSuit/Joker.png',
+  name: 'Джокер',
+  price: '1500',
+  stock: 1
+},
+{
+  id: '2673077791',
+  image: 'imageSuit/SuperMan.png',
+  name: 'Супермен',
+  price: '700',
+  stock: 4
+},
+{
+  id: '2983647791',
+  image: 'imageSuit/Obi.png',
+  name: 'Оби Ван Кеноби',
+  price: '1000',
+  stock: 1
+},
+{
+  id: '2128979153',
+  image: 'imageSuit/spiderMan.png',
+  name: 'Человек-паук',
+  price: '1000',
+  stock: 1
+},
+{
+  id: '2363897913',
+  image: 'imageSuit/Batman.png',
+  name: 'Бэтмен',
+  price: '1500',
+  stock: 0
 },
 ]
